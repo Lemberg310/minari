4299ef90c2e1b3582b8f6193291e5cc02ca93662
Lemberg310 <63932812+Lemberg310@users.noreply.github.com>
2020-07-25 22:01:03 +0300

    Update minari.py

--- minari.py
+++ minari.py
@@ -24,7 +24,7 @@ async def on_ready():
 	print(' $ Bot in system')
 
 @client.event
-async def on_message(txt):
+async def on_message(message, txt):
 	for i in list_sad:
 		if i in txt.content:
 			await message.edit("бзб , кидай сюда, братанчик - +380689010677 QIWI")
